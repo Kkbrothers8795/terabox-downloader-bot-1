93c9ff68c30da7b39728b2de43c8d53a91ac122f
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-07-09 10:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BOT_TOKEN = "6850653040:AAHGdG1zUvet28XQqe126Y9R-85Y4Tuvip4"  # bot token
 
 
 ## REDIS
-HOST = "uncommon-lemur-53593.upstash.io"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = "AdFZAAIncDFkOGQzOGU5MjU2OWY0ZDZkYmM4YmM2YWFiZjgyNmQxMnAxNTM1OTM"  # redis password
+HOST = "redis-17383.c330.asia-south1-1.gce.redns.redis-cloud.com"  # redis host uri
+PORT = "17383"  # redis port
+PASSWORD = "Rxd3It8n6CJW1Zc1Q4UsAzZ7uTpTCLfi"  # redis password
 
 PRIVATE_CHAT_ID = -1001478442561  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = "PANWEB=1; csrfToken=GHCr5c7PG7m-uNmwgZMs_rTm; browserid=IpOTkn3sUXflH137gkjAABKPpYQPOkWv7DF2TkYb-z90IFGy4K3S4fbqSHw=; lang=en; __bid_n=1904cc18665ad49a7a4207; __stripe_mid=59251633-d0b6-45e9-8f1e-60eb717f07333c544e; ndus=YvV4We4teHuiGZaml4vV_hxd2Xil7cVxPm0uO_da; ndut_fmt=E6F216812868AD4BF4002916042687B822BD9F49DD31E62DF500A343018F7069; ab_sr=1.0.1_ZjEyOTI2NDY3ZDFmYTcwYzBhMjgzODVlZjNkM2ZjNDAyNTJjYjIzOTVkNWY1MzliNDkxZDU5YjllOTA5ZDdjZjgwOTdmZjk0NWI3MzZlOTJkNmUwNzcwYzc4NjhlZGZjNDcwMjMwZWU0NjM3MTFkOGEzNDg5YmMyY2RlNWQ4YWNlNGEyMjhjOTgyYzdiNjkwNjI3YzE4Y2FhMTEyMDBmYQ==;  _ga=GA1.1.1388011841.1719274827; _ga_06ZNKL8C2E=GS1.1.1719274826.1.1.1719275017.60.0.0"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [754495556]
+ADMINS = "754495556"
